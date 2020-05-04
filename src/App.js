c799eeb76513ec3c90db6c955c6c1e6ea7777423
Yann <devilng05108@gmail.com>
2020-05-04 12:47:31 +0800

    React initialization

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route path="/register" component={Register}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/profile" component={Profile}/>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      role: 'admin',
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.login();
+  };
+
+  login() {
+    axios
+      .post('users/loginwithrole', {
+        email: this.state.email,
+        password: this.state.password,
+        role: this.state.role,
+      })
+      .then((res) => {
+
+        if (res.data.error) {
+          console.log("Error : " + res.data.error);
+        } 
+        else {
+          console.log(res.data.token);
+          localStorage.setItem('usertoken', res.data.token);
+          console.log('Successfully logging in');
+          this.props.history.push('/profile');
+        }
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mt-5 mx-auto">
+            <form noValidate onSubmit={this.onSubmit}>
+              <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+              <div className="form-group">
+                <label htmlFor="email">Email Address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder="Enter email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="Enter password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="role">Role</label>
+                <select
+                  className="form-control"
+                  name="role"
+                  value={this.state.role}
+                  onChange={this.onChange}
+                >
+                  <option value="admin">Admin</option>
+                  <option value="student">
+                    Student
+                  </option>
+                  <option value="teacher">Teacher</option>
+                  <option value="librarian">Librarian</option>
+                </select>
+              </div>
+              <button
+                type="submit"
+                className="btn btn-lg btn-primary btn-block"
+              >
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
