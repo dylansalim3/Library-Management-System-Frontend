06379c8381ab318db751ddca1e8ea1a0e70e82c8
dylan <dylansalim3@hotmail.com>
2020-10-02 16:36:48 +0800

    edit service worker

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,11 @@
 function receivePushNotification(event) {
     console.log("[Service Worker] Push Received.");
 
-    // console.log(event.data);
-    // const { image, tag, url, title, text } = event.data.json();
+    const { image, tag, url, title, text } = event.data?.json();
 
-    // const options = {
-    //     data: url,
-    //     body: text,
-    //     icon: image,
-    //     vibrate: [200, 100, 200],
-    //     tag: tag,
-    //     image: image,
-    //     badge: "https://spyna.it/icons/favicon.ico",
-    //     actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
-    // };
-
-    const options = {
-        data: 'f',
-        body: 'dsa',
+    let options = {
+        data: 'localhost:3000',
+        body: 'New Message from E-Library',
         icon: 'https://sites.google.com/site/thisisjustatest2294/_/rsrc/1468742544208/project-resources/image-search/google-image-search/Screen%20Shot%202015-11-28%20at%201.14.27%20PM.png',
         vibrate: [200, 100, 200],
         tag: 'a',
@@ -25,7 +13,21 @@ function receivePushNotification(event) {
         badge: "https://spyna.it/icons/favicon.ico",
         actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
     };
-    event.waitUntil(self.registration.showNotification("csnj", options));
+
+    if(event.data){
+        options = {
+            data: url,
+            body: text,
+            icon: image,
+            vibrate: [200, 100, 200],
+            tag: tag,
+            image: image,
+            badge: image,
+            actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
+        };
+    }
+
+    event.waitUntil(self.registration.showNotification(title, options));
 }
 
 function openPushNotification(event) {
